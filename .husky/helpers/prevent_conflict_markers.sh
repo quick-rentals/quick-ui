ab9d73c32be3a1b27b671ed96283fa7f31c0d1d9
abdulbasitha <basith.ambalavan@gmail.com>
2023-10-21 00:08:51 +0530

    husky config updates

---
+++ .husky/helpers/prevent_conflict_markers.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+
+repeat() {
+  char="$1"
+  times="$2"
+  text=""
+  i=0
+
+  while [ $i -lt $times ]
+  do
+    text="${text}${char}"
+    i=$((i+1))
+  done
+  echo "$text"
+}
+
+prevent_conflict_markers() {
+  TERM="xterm-256color"
+  RED=$(tput setab 1)
+  NORMAL=$(tput sgr0)
+
+  left_arrow=$(repeat '<' 7)
+  right_arrow=$(repeat '>' 7)
+  equal_symbol=$(repeat '=' 7)
+
+  CONFLICT_MARKERS="${left_arrow}|${equal_symbol}|${right_arrow}"
+  occurrences=($(git --no-pager diff --staged --ignore-blank-lines --name-only -G"$CONFLICT_MARKERS"))
+
+  for file in "${occurrences[@]}"; do
+    occurrences_count=$(grep -Ev '^[[:space:]]*#' "$file" | grep -E "$CONFLICT_MARKERS" | wc -l)
+    if [ "$occurrences_count" -gt 0 ]; then
+      echo "$RED ERROR $NORMAL Found ${CONFLICT_MARKERS} symbols in staged files."
+      echo "Conflict markers should be removed from the following files before committing:"
+      git --no-pager diff --staged --ignore-blank-lines --name-only -G"$CONFLICT_MARKERS"
+      exit 1;
+    fi
+  done
+}
