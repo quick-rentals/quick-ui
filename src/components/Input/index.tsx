bd7e5737ffb4c33b0992f6ea75ab6f8fd78a39d2
abdulbasitha <basith.ambalavan@gmail.com>
2023-10-24 13:26:02 +0530

    chore:config changes

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,35 @@
+interface InputProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: "small" | "medium" | "large";
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+const Input = ({
+}: InputProps) => {
+  return (
+    <input type="text" name="hello"/>
+  
+  );
+};
+
+export default Input;
---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+import Button from "./Button";
+import Input from "./Input";
+// scss file imports
+import "../styles/index.scss";
+
+export {
+    Input,
+    Button
+  // Button related components
+
+};
